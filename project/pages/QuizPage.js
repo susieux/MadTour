85b7c36d0acbc1295e87c713606d1c027a955300
Codey Severance <48614070+IronnButterfly@users.noreply.github.com>
2020-03-27 19:54:30 -0500

    basic (non-working) skeleton of the QuizPage

--- project/pages/QuizPage.js
+++ project/pages/QuizPage.js
@@ -1,23 +1,46 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import Slider from 'react-native-slider';
 import { ScrollView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import styleInfo from '../styleInfo.js';
+
+const quizStack = createStackNavigator();
+
+const width = Dimensions.get('window');
 
 export default function QuizPage() {
     return (
         <View style={styles.container}>
-            <View>
-                <Text>
-                    The Quiz
-                </Text>
-            </View>
-            {qtSlider()}
+            <Text style={styles.mainText}>
+                The Quiz
+            </Text>
+            <ScrollView
+                style={styles.container}
+                horizontal={true}
+                //these values need to be changed and tweaked
+                snapToInterval={width - 60}
+                snapToAlignment={"center"} 
+                contentInset={{
+                    top:0,
+                    left:30,
+                    bottom:0,
+                    right:30,
+                }}
+            >
+                {qIntensity()}
+                {qDistance()}
+                {qDoorness()}
+                {qTransport()}
+                {qTimeFrame()}
+            </ScrollView>
         </View>
     );
 }
 
+{// contains qt code functions
 /**
  * Used to generalize questions that can have the input of Sliders
  * 
@@ -33,20 +56,27 @@ export default function QuizPage() {
  * @param {Number} interval 
  * The distance between each "notch" in the slider using the same units as start and end
  */
-function qtSlider(question="Default Question", start=0, end=10, interval=1, startText="0", endText="10") {
-    //the formatting of the
+function qtSlider(question = "Default Question", left = 0, right = 10, start = 5, interval = 1, lText = "0", rText = "10") {
     return (
         <View>
             <Text>
                 {question}
             </Text>
-            <Slider 
-                minimumValue={start}
-                maximumValue={end}
+            <Slider
+                minimumValue={left}
+                maximumValue={right}
+                value={start}
                 step={interval}
             >
-                Some text in the slider
             </Slider>
+            <Text>
+                {lText}
+                Should be on the left
+            </Text>
+            <Text>
+                {rText}
+                should be on the right
+            </Text>
         </View>
     );
 }
@@ -63,10 +93,23 @@ function qtSlider(question="Default Question", start=0, end=10, interval=1, star
 function qtRadio(question, ans) {
 
 }
+}// contains qt code functions
 
+{//contains q code functions
 //slider
+/**
+ * Call to qtSlider with the necessry info in the params to ask the question.
+ * Question: "How active do you wish to be on this trip?"
+ */
 function qIntensity() {
-
+    let q = "How active do you wish to be on this trip";
+    let l = 1;
+    let r = 10;
+    let s = 5;
+    let i = 1;
+    let lText = "Relaxed";
+    let rText = "Busy";
+    qtSlider(q,l,r,s,i,lText,rText);
 }
 
 //slider
@@ -89,5 +132,6 @@ function qTransport() {
 function qTimeFrame() {
 
 }
+}//contains q code functions
 
 const styles = StyleSheet.create(styleInfo);
