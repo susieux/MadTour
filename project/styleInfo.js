70ebac497d4856e4da3ee59c967c95b5ead0f08e
Codey Severance <48614070+IronnButterfly@users.noreply.github.com>
2020-04-17 14:07:29 -0500

    small formatting to styleInfo

--- project/styleInfo.js
+++ project/styleInfo.js
@@ -1,16 +1,30 @@
 //all default style information used in the formatting of the pages
 import { Dimensions } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 const w = Dimensions.get('screen').width;
+
+const colors = {
+    bg: '#fad1d1', //light pink for backgrounds
+    bgMid: '#ff9999', //darker pink used as a middle red for backgrounds
+    bgAccent: '#e57373', //even darker red to accent the bgMid color
+    mainFill: '#c62828', //main red for logos and important pieces
+    white:'#efefef', //a very off white to be used in place of white
+    black:'#101010',
+    subBlack:'#202020',
+    debugGreen:'#0f0',
+    debugBlue:'#00f',
+    debugYellow:'#ff0',
+}
 
 export default {
     //main view
     //the whole page
     container: {
         flex: 1,
-        backgroundColor: '#d32f2f',
+        backgroundColor: colors.bg,
         padding: 20,
         //using border for debugging
-        borderColor:'#fff',
+        borderColor:colors.debugGreen,
         borderWidth: 2,
     },
     //view inside the view
@@ -21,61 +35,61 @@ export default {
         paddingHorizontal: 0,
         alignContent: 'center',
         //using border for debugging
-        borderColor:'#b8b8b8',
+        borderColor:colors.debugYellow,
         borderWidth:2,
     },
     //Container within the contentContainer
     subContainer: {
-        backgroundColor: '#b8b8b8',
+        backgroundColor: colors.bgMid,
         width: w*.8,
         flex:1,
         padding: 30,
         //using border for debugging
-        borderColor:'#efefef',
+        borderColor:colors.debugBlue,
         borderWidth:2,
     },
     titleText: {
         fontSize: 45,
-        color: '#222',
+        color: colors.black,
         textAlign: 'center',
     },
     //title text
     //header 1
     mainText: {
         fontSize: 20,
-        color: '#222',
+        color: colors.black,
         textAlign: 'center',
     },
     //"sub" title text
     //header2
     subText: {
         fontSize: 15,
-        color: '#444',
+        color: colors.subBlack,
         textAlign: "center",
     },
     //content text
     //header 3
     text: {
         fontSize: 10,
-        color: '#666',
+        color: colors.black,
         textAlign: 'center',
     },
     //text for the buttons
     buttonText: {
         fontSize: 23,
-        color: '#222',
+        color: colors.subBlack,
         textAlign: "center",
     },
     eventText: {
         fontSize: 23,
-        color: '#222',
+        color: colors.subBlack,
         textAlign: 'left',
         marginBottom: 15,
         fontWeight: 'bold'
     },
     buttonEventText: {
         fontSize: 23,
-        color: '#222',
+        color: colors.subBlack,
         textAlign: 'left',
         marginBottom: 15,
         fontWeight: 'bold',
@@ -83,7 +97,7 @@ export default {
     },
     //button formatting
     button: {
-        backgroundColor: '#b8b8b8',
+        backgroundColor: colors.bgAccent,
         paddingVertical: 20,
         borderRadius: 100,
         marginBottom: 20,
@@ -119,6 +133,7 @@ export default {
     },
     images: {
         transform: [{scale: .7}],
-        marginLeft: -w/5.5
+        marginLeft: -w/5.5,
+        marginRight: w/5.5,
     }
 };
